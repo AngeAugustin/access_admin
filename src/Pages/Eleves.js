d0ed8bcdb2aa0dc92c92499027073663b5c92fea
AngeAugustin <101171968+AngeAugustin@users.noreply.github.com>
2025-03-17 14:43:02 +0100

    Eleves update

--- src/Pages/Eleves.js
+++ src/Pages/Eleves.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Eleves = () => {
+  const [enfants, setEnfants] = useState([]);
+
+  const avatarGarcon = "https://i.postimg.cc/7Yks2MRJ/gar-on.jpg";
+  const avatarFille = "https://i.postimg.cc/6p6MH6t9/fille.jpg";
+
+  // Récupérer les produits depuis l'API
+  useEffect(() => {
+    fetch(`https://access-backend-a961a1f4abb2.herokuapp.com/api/get_all_enfants`)
+      .then((res) => res.json())
+      .then((data) => {
+        setEnfants(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div style={{ padding: "20px" }}>
       {/* Header section */}
       <div style={{ marginBottom: "20px" }}>
-        <h2 style={{ margin: 0, fontSize: 16 }}>Elèves</h2>
+        <h2 style={{ margin: 0, fontSize: 16 }}>Élèves</h2>
         <p style={{ color: "#555", margin: 0, paddingTop: 8, fontSize: 14 }}>
           Gérer les élèves
         </p>
@@ -14,29 +29,13 @@ const Eleves = () => {
 
       <div style={{ height: "5px" }}></div>
 
-      {/* Line separator with lighter color and thinner width */}
+      {/* Line separator */}
       <div style={{ borderBottom: "1px solid #ddd", marginBottom: "20px" }}></div>
 
       <div style={{ height: "10px" }}></div>
 
       {/* List of students */}
-      {[
-        {
-          name: "KOLA Saroo",
-          classe: "CM2",
-          avatar: "https://via.placeholder.com/50",
-        },
-        {
-          name: "ILOSIWAJU Bernice",
-          classe: "1ère",
-          avatar: "https://via.placeholder.com/50",
-        },
-        {
-          name: "XALOGUN Bashorun",
-          classe: "2nde",
-          avatar: "https://via.placeholder.com/50",
-        },
-      ].map((student, index) => (
+      {enfants.map((enfant, index) => (
         <div
           key={index}
           style={{
@@ -53,28 +52,32 @@ const Eleves = () => {
         >
           <div style={{ display: "flex", alignItems: "center" }}>
             <img
-              src={student.avatar}
+              src={enfant.Sexe_enfant === "M" ? avatarGarcon : avatarFille}
               alt="avatar"
               style={{ width: 50, height: 50, borderRadius: "50%", marginRight: "10px" }}
             />
             <div>
-              <p style={{ margin: 0, fontWeight: "bold" }}>{student.name}</p>
-              <p style={{ margin: 0, color: "#666", fontSize: "14px" }}>{student.classe}</p>
+              <p style={{ margin: 0, fontWeight: "bold" }}>
+                {enfant.Nom_enfant} {enfant.Prenom_enfant}
+              </p>
+              <p style={{ margin: 0, color: "#666", fontSize: "14px" }}>
+                {enfant.Classe_actuelle}
+              </p>
             </div>
           </div>
-          <Link to="/DetailsEleve">
-          <button
-            style={{
-              background: "#004aad",
-              color: "white",
-              border: "none",
-              padding: "8px 12px",
-              borderRadius: "5px",
-              cursor: "pointer",
-            }}
-          >
-            Voir les détails
-          </button>
+          <Link to={`/DetailsEleve/${enfant.NPI_enfant}`}>
+            <button
+              style={{
+                background: "#004aad",
+                color: "white",
+                border: "none",
+                padding: "8px 12px",
+                borderRadius: "5px",
+                cursor: "pointer",
+              }}
+            >
+              Voir les détails
+            </button>
           </Link>
         </div>
       ))}
